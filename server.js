405d7aaede0550c4a7586d19456c7606d2d31d4d
Logan <logankeyes1212@gmail.com>
2019-03-04 22:41:58 -0600

    changes to package.json

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+let PORT = process.env.PORT;
+if (PORT == null || PORT == "") {
+  PORT = 8000;
+}
+// app.listen(port);
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
